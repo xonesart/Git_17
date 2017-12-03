31825f417d34d34d4772f5c96ba209831e4b3479
Andre <33806558+xonesart@users.noreply.github.com>
2017-12-03 10:54:27 +0100

    4w

---
+++ P17-Git-master/src/nowy_pakiet/Cztery.java
@@ -0,0 +1,12 @@
+package nowy_pakiet;
+
+public class Cztery {
+
+	public static void main(String[] args) {
+		WersjaTrzecia obiekt = new WersjaTrzecia();
+		obiekt.setImie("Ala");
+		obiekt.setNazwisko("Ola");
+		System.out.println(obiekt);
+	}
+
+}
