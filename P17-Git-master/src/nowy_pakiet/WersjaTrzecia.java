594f5cbbc97dc2be5adedd168b60256c4333b1b3
Andre <33806558+xonesart@users.noreply.github.com>
2017-12-03 10:54:17 +0100

    3w

---
+++ P17-Git-master/src/nowy_pakiet/WersjaTrzecia.java
@@ -0,0 +1,57 @@
+package nowy_pakiet;
+
+public class WersjaTrzecia {
+	private String imie, nazwisko;
+
+	public String getImie() {
+		return imie;
+	}
+
+	public void setImie(String imie) {
+		this.imie = imie;
+	}
+
+	public String getNazwisko() {
+		return nazwisko;
+	}
+
+	public void setNazwisko(String nazwisko) {
+		this.nazwisko = nazwisko;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((imie == null) ? 0 : imie.hashCode());
+		result = prime * result + ((nazwisko == null) ? 0 : nazwisko.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WersjaTrzecia other = (WersjaTrzecia) obj;
+		if (imie == null) {
+			if (other.imie != null)
+				return false;
+		} else if (!imie.equals(other.imie))
+			return false;
+		if (nazwisko == null) {
+			if (other.nazwisko != null)
+				return false;
+		} else if (!nazwisko.equals(other.nazwisko))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "WersjaTrzecia [imie=" + imie + ", nazwisko=" + nazwisko + "]";
+	}
+}
